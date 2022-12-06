b3778fbb1fe5a0c68499d2cc02ec473c378dcdbf
Jonathan <49077851+Lock-The-Door@users.noreply.github.com>
2022-12-06 12:29:21 -0500

    Add eat sound

--- Food.java
+++ Food.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Food extends Actor
 {
+    GreenfootSound eatSound = new GreenfootSound("./sounds/elephantcub.mp3");
+
     /**
      * Act - do whatever the food wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -27,6 +29,9 @@ public class Food extends Actor
     
     public void eat()
     {
+        // play noise
+        eatSound.play();
+
         // respawn apple at top
         setLocation(Greenfoot.getRandomNumber(getWorld().getWidth()), 1);
 
